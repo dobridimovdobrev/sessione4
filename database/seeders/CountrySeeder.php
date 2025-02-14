c5dce592539b3ed78f7ca809211211f0f7740704
Dobri Dobrev <dobri_dobrev@yahoo.com>
2025-02-14 20:52:54 +0100

    adding file scv nations

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -12,22 +12,16 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        $file = fopen(storage_path('storage/app/csv/nazioni.csv'), 'r');
-        
-        // Salta l'header
-        fgetcsv($file);
-        
-        while (($data = fgetcsv($file)) !== false) {
-            Country::create([
-                'country_id' => $data[0],
-                'name' => $data[1],
-                'continent' => $data[2],
-                'iso_char2' => $data[3],
-                'iso_char3' => $data[4],
-                'phone_prefix' => $data[5]
-            ]);
+        $countries = [
+            ['country_id' => 1, 'name' => 'Bulgaria', 'continent' => 'Europe', 'iso_char2' => 'BG', 'iso_char3' => 'BGR', 'phone_prefix' => '+359'],
+            ['country_id' => 2, 'name' => 'Italy', 'continent' => 'Europe', 'iso_char2' => 'IT', 'iso_char3' => 'ITA', 'phone_prefix' => '+39'],
+            ['country_id' => 3, 'name' => 'United States', 'continent' => 'North America', 'iso_char2' => 'US', 'iso_char3' => 'USA', 'phone_prefix' => '+1'],
+            ['country_id' => 4, 'name' => 'United Kingdom', 'continent' => 'Europe', 'iso_char2' => 'GB', 'iso_char3' => 'GBR', 'phone_prefix' => '+44'],
+            ['country_id' => 5, 'name' => 'Germany', 'continent' => 'Europe', 'iso_char2' => 'DE', 'iso_char3' => 'DEU', 'phone_prefix' => '+49'],
+        ];
+
+        foreach ($countries as $country) {
+            Country::create($country);
         }
-        
-        fclose($file);
     }
 }
