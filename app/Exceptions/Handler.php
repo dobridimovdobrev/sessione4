c9c7e60329737244b10ccf65033dd9c0306c9bea
Dobri Dobrev <dobri_dobrev@yahoo.com>
2025-08-17 15:02:21 +0200

    api route login issue fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,6 +36,14 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    /**
+     * Convert an authentication exception into a response.
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
 
     /**
      * Register the exception handling callbacks for the application.
